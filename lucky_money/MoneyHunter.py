36e5d77a5fb0c36e3c08f0b5aad397238c26346d
zhenxun815 <guoyiheng89@gmail.com>
2020-01-15 17:51:47 +0800

    add into listen room and watch the fly notice

--- lucky_money/MoneyHunter.py
+++ lucky_money/MoneyHunter.py
@@ -28,7 +28,7 @@ class MoneyHunter:
                 'platformVersion': '5.1.1'
         }
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(30)
 
     def login(self):
         print('into login step...')
@@ -54,16 +54,32 @@ class MoneyHunter:
 
         print(f'into home activity..')
         print(f'confirm the youth mode notice..')
-        driver_utils.waiting_element(self.driver, 'com.netease.play:id/youthModeBtn') \
-            .click()
+        driver_utils.waiting_element(self.driver,
+                                     'com.netease.play:id/youthModeBtn',
+                                     interval=3).click()
 
         # when update version notice appear, simulate pressing the back key
-        driver_utils.waiting_element(self.driver, 'com.netease.play:id/updateVersionContent')
+        driver_utils.waiting_element(self.driver,
+                                     'com.netease.play:id/updateVersionContent',
+                                     interval=10)
         self.driver.keyevent(4)
         print('cancel version update...')
 
+    def to_listen(self):
+        print('into listen rooms')
+        rooms = driver_utils.waiting_elements(self.driver, 'com.netease.play:id/homecard')
+        print(f'room num is {len(rooms)}')
+        rooms[0].click()
+
+    def watch_fly(self):
+        print('start watching fly')
+        notice_container = driver_utils.waiting_element(self.driver, 'com.netease.play:id/liveNoticeContainer')
+        notice = self.driver.find_element_by_id('com.netease.play:id/liveNotice')
+        print(f'fly text is {notice.text}')
+        notice_container.click()
 
 
 if __name__ == '__main__':
     hunter1 = MoneyHunter('15255926026', '890815')
     hunter1.login()
+    hunter1.to_listen()

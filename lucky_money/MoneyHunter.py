f2cf9e6a1133b47987a6d232c4b3a76064549b3f
zhenxun815 <guoyiheng89@gmail.com>
2020-01-17 11:09:27 +0800

    update

--- lucky_money/MoneyHunter.py
+++ lucky_money/MoneyHunter.py
@@ -9,12 +9,13 @@
 # @Time: 1/15/2020 12:04
 import time
 from lucky_money import driver_utils
-
+from datetime import datetime
 from appium import webdriver
+from appium.webdriver.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class MoneyHunter:
-
     memu_desired_caps = {
             'platformName':    'Android',
             'deviceName':      '127.0.0.1:21503',
@@ -24,14 +25,14 @@ class MoneyHunter:
     }
 
     miaomi_desired_caps = {
-            "platformName":           "Android",
-            "deviceName":             "bf005899",
-            "appPackage":             "com.netease.play",
-            "appActivity":            "com.netease.play.appstart.LoadingActivity",
-            "platformVersion":        "9",
-            "mjpegScreenshotUrl":     "http://192.168.1.214:8080/stream.mjpeg",
-            "automationName":         "UiAutomator2",
-            "ignoreUnimportantViews": True
+            'platformName':           'Android',
+            'deviceName':             'bf005899',
+            'appPackage':             'com.netease.play',
+            'appActivity':            'com.netease.play.appstart.LoadingActivity',
+            'platformVersion':        '9',
+            # 'mjpegScreenshotUrl':     'http://192.168.1.214:8080/stream.mjpeg',
+            # 'automationName':         'UiAutomator2',
+            'ignoreUnimportantViews': True
     }
 
     def __init__(self, phone_number, password, is_emulator):
@@ -41,7 +42,13 @@ class MoneyHunter:
         self.is_emulator = is_emulator
         desired_caps = self.memu_desired_caps if is_emulator else self.miaomi_desired_caps
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-        self.driver.implicitly_wait(30)
+
+    def auth(self):
+        print('start auth...')
+        driver_utils.waiting_element(self.driver, 'com.netease.play:id/grant').click()
+        driver_utils.waiting_element(self.driver, 'android:id/button1').click()
+        time.sleep(2)
+        self.driver.find_element_by_id('android:id/button1').click()
 
     def login(self):
         print('into login step...')
@@ -52,7 +59,7 @@ class MoneyHunter:
         print('start choose land mode...')
         # tap at [], to confirm the agreement
         driver_utils.waiting_element(self.driver, 'com.netease.play:id/agreement')
-        tap_point = (170, 1312) if self.is_emulator else (170, 1312)
+        tap_point = (170, 1312) if self.is_emulator else (191, 1826)
         self.driver.tap([tap_point], 500)
 
         # choose land mode
@@ -94,21 +101,22 @@ class MoneyHunter:
         self.driver.find_elements_by_class_name('androidx.appcompat.app.ActionBar$Tab')
 
     def watch_fly(self):
-        print('start watching fly...')
 
-        try:
-            notice_container = self.driver.find_element_by_id('com.netease.play:id/liveNoticeContainer')
-            # notice = self.driver.find_element_by_id('com.netease.play:id/liveNotice')
-            # print(f'fly text is {notice.text}')
-            notice_container.click()
-        except Exception as e:
-            print(e)
+        self.driver.implicitly_wait(10)
+        print('start watching fly...')
+        while True:
+            print(f'tap {datetime.today()}')
+            time.sleep(1)
+            tap_point = (700, 180) if self.is_emulator else (900, 250)
+            self.driver.tap([tap_point], 500)
 
 
 if __name__ == '__main__':
 
-    hunter1 = MoneyHunter('15255926026', '890815', is_emulator=False)
+    is_emulator = False
+    hunter1 = MoneyHunter('15255926026', '890815', is_emulator=is_emulator)
+    if not is_emulator:
+        hunter1.auth()
     hunter1.login()
     hunter1.to_listen()
-    while True:
-        hunter1.watch_fly()
+    hunter1.watch_fly()

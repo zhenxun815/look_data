0b2cd2050d6e30e0d211e1576c6292322f306ef2
zhenxun815 <guoyiheng89@gmail.com>
2020-01-19 16:42:09 +0800

    update

--- lucky_money/driver_utils.py
+++ lucky_money/driver_utils.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# @Description: 
+# @Description:
 # @File: driver_utils.py
 # @Project: look_data
 # @Author: Yiheng
 # @Email: GuoYiheng89@gmail.com
 # @Time: 1/15/2020 15:43
-from appium.webdriver.webdriver import WebDriver
 from datetime import datetime
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def ec_element_clickable(element_id: str):
@@ -43,36 +42,27 @@ def ec_elements_present(element_id: str):
     return EC.presence_of_all_elements_located(locator)
 
 
-def waiting_clickable(driver: WebDriver, element_id: str, max_wait=30, interval=1):
-    return WebDriverWait(driver, timeout=max_wait, poll_frequency=interval).until(
-            ec_element_clickable(element_id))
+def waiting_clickable(waiter: WebDriverWait, element_id: str):
+    return waiter.until(ec_element_clickable(element_id))
 
 
-def waiting_element(driver: WebDriver, element_id: str, max_wait=30, interval=1):
+def waiting_element(waiter: WebDriverWait, element_id: str):
     """
     waiting element until it present, default max waiting time is 30 seconds,
     default poll interval is 1 seconds.
-    :param driver:
+    :param waiter:
     :param element_id:
-    :param max_wait: unit is second
-    :param interval:
     :return:
     """
-    WebDriverWait(driver, timeout=max_wait, poll_frequency=interval).until(
-            ec_element_present(element_id))
-    return driver.find_element_by_id(element_id)
+    return waiter.until(ec_element_present(element_id))
 
 
-def waiting_elements(driver: WebDriver, element_id: str, max_wait=30, interval=5):
+def waiting_elements(waiter: WebDriverWait, element_id: str):
     """
     waiting element until it present, default max waiting time is 30 seconds,
     default poll interval is 5 seconds.
-    :param driver:
+    :param waiter:
     :param element_id:
-    :param max_wait: unit is second
-    :param interval:
     :return:
     """
-    WebDriverWait(driver, timeout=max_wait,
-                  poll_frequency=interval).until(ec_elements_present(element_id))
-    return driver.find_elements_by_id(element_id)
+    return waiter.until(ec_elements_present(element_id))
